2441ad3dea9acae062ae06efb3244f3a1845d8aa
Hjibhakate <hjibhakte2004@gmail.com>
2026-06-20 18:16:12 +0530

    new open change

--- agent.py
+++ agent.py
@@ -1,7 +1,8 @@
 from dotenv import load_dotenv
+import os
 
 from livekit.agents import Agent, AgentSession, JobContext, WorkerOptions, cli
-from livekit.plugins import google, deepgram, cartesia, silero
+from livekit.plugins import openai, deepgram, cartesia, silero
 
 from prompts import FUNNY_FRIEND_PROMPT
 
@@ -18,7 +19,14 @@ class FunnyFriend(Agent):
 async def entrypoint(ctx: JobContext):
     session = AgentSession(
         stt=deepgram.STT(),
-        llm=google.LLM(model="gemini-2.5-flash"),
+
+        # OpenRouter AI Brain
+        llm=openai.LLM(
+            model="openai/gpt-4o-mini",
+            api_key=os.getenv("OPENROUTER_API_KEY"),
+            base_url="https://openrouter.ai/api/v1",
+        ),
+
         tts=cartesia.TTS(),
         vad=silero.VAD.load(),
     )
